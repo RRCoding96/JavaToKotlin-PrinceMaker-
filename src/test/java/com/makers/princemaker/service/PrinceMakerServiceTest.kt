66bf6141b1a34d423043d8ca60515de6d5026a7f
김윤욱 <reznov03048@gmail.com>
2024-09-22 22:36:29 +0900

    실습 - DTO dummy 생성 및 적용, enum 통합

--- src/test/java/com/makers/princemaker/service/PrinceMakerServiceTest.kt
+++ src/test/java/com/makers/princemaker/service/PrinceMakerServiceTest.kt
@@ -4,6 +4,7 @@ import com.makers.princemaker.code.PrinceMakerErrorCode
 import com.makers.princemaker.code.StatusCode
 import com.makers.princemaker.constant.PrinceMakerConstant
 import com.makers.princemaker.controller.CreatePrince
+import com.makers.princemaker.dto.dummyCreatePriceRequest
 import com.makers.princemaker.entity.Prince
 import com.makers.princemaker.entity.PrinceMock
 import com.makers.princemaker.entity.dummyPrince
@@ -70,13 +71,10 @@ internal class PrinceMakerServiceTest {
     @Test
     fun createPrinceTest_success() {
         //given
-        val request = CreatePrince.Request(
-            PrinceLevel.MIDDLE_PRINCE,
-            SkillType.INTELLECTUAL,
-            7,
-            "princeId",
-            "name",
-            28
+        val request = dummyCreatePriceRequest().copy(
+            experienceYears = 7,
+            princeLevel = PrinceLevel.MIDDLE_PRINCE,
+            skillType = SkillType.INTELLECTUAL
         )
         val slot = slot<Prince>()
         every {
